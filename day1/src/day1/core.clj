4e2b1bab8dd030f0cdb9c5e93ed9cfbeeee2f306
jonas <jonasj9@kth.se>
2017-12-15 16:06:34 +0100

    day 1 done

--- day1/src/day1/core.clj
+++ day1/src/day1/core.clj
@@ -2,27 +2,31 @@
 
 (defn sequense
   "input sequens transformed into int-collection"
-  [input-file]
-  (->> (slurp input-file)
+  [path-to-file]
+  (->> (slurp path-to-file)
        (seq)
        (map (fn [char]
               (Character/getNumericValue char)))))
 
 (defn filter-equals
-  [seq step]
-  (as->(keep-indexed (fn [num item]
-                  (if (= (mod num step) 0)
-                    item)) seq) $
-       (for [i (range 1 (count $))]
-         (if (= (nth $ i) (nth $ (- i 1)))
-           (nth $ i)
-           0
-           ))
-     ))
-
-(reduce + (filter-equals (concat (sequ) (sequ)) (/ (count (sequ)) 2 )))
+  [sequ step]
+  (let [second-iterator (fn [index]
+                          (mod (+ index step) (count sequ)))]
+    (map-indexed (fn [index item]
+                   (if (= item (nth sequ (second-iterator index)))
+                     item
+                     0)) sequ)))
 
 
+;; Answer 1
+(as-> (sequense "/home/jonas/development/advent-of-code-2017/day1/src/day1/sequense") $
+    (filter-equals $ 1)
+    (reduce + $))
+
+;;Answer 2
+(as-> (sequense "/home/jonas/development/advent-of-code-2017/day1/src/day1/sequense") $
+      (filter-equals $ (/ (count $) 2))
+      (reduce + $))
 
 
 
