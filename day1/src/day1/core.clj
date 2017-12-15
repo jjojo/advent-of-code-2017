ab5b086621f462fd1953fde3323846ca6d1bfe01
jonas <jonasj9@kth.se>
2017-12-15 16:09:59 +0100

    day 1 done

--- day1/src/day1/core.clj
+++ day1/src/day1/core.clj
@@ -1,19 +1,22 @@
 (ns day1.core)
 
+
 (defn sequense
-  "input sequens transformed into int-collection"
+  "input sequence transformed into int-collection"
   [path-to-file]
   (->> (slurp path-to-file)
        (seq)
        (map (fn [char]
               (Character/getNumericValue char)))))
 
+
 (defn filter-equals
+  "saves equal elements (by step apart), sets unequal elements to 0"
   [sequ step]
   (let [second-iterator (fn [index]
                           (mod (+ index step) (count sequ)))]
-    (map-indexed (fn [index item]
-                   (if (= item (nth sequ (second-iterator index)))
+    (map-indexed (fn [first-iterator item]
+                   (if (= item (nth sequ (second-iterator first-iterator)))
                      item
                      0)) sequ)))
 
